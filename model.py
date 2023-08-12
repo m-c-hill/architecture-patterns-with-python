38f6a25fc3913f10e5ee431edb1d542cd2813ffe
Matt Hill <matthewhill2595@gmail.com>
2023-08-12 08:11:03 +0100

    allocation function and associated tests

--- model.py
+++ model.py
@@ -18,15 +18,31 @@ class Batch:
     """
     A batch is a quantity of a specific product to be ordered by the the purchasing department. A
     batch of stock is identified by a reference id. When a customer creates an order, the order
-    lines associated with the order are allocated to a batch with a matching sku.
+    lines associated with the order are allocated to a batch with a matching sku. Batches have an
+    eta if they are currently shipping. If eta = None, then they are currently in the warehouse.
     """
 
-    def __init__(self, reference: str, sku: str, eta: date, quantity: int):
+    def __init__(self, reference: str, sku: str, quantity: int, eta: date):
         self.reference = reference
         self.sku = sku
         self.eta = eta
         self._total_quantity = quantity
         self._allocated_order_lines: set[OrderLine] = set()
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return False
+        return self.eta > other.eta
 
     def allocate(self, order_line: OrderLine) -> None:
         if self.can_allocate(order_line):
@@ -45,4 +61,25 @@ class Batch:
         return self._total_quantity - self.allocated_quantity
 
     def can_allocate(self, order_line: OrderLine) -> bool:
-        return order_line.sku == self.sku and self.available_quantity >= order_line.quantity
+        return (
+            order_line.sku == self.sku
+            and self.available_quantity >= order_line.quantity
+        )
+
+
+def allocate(line: OrderLine, batches: list[Batch]) -> str:
+    """
+    Allocate a line to a batch in a list of batches, with preference for earlier batches
+    """
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
+class OutOfStock(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,8 @@
-from datetime import date
-
-from model import Batch, OrderLine
+from datetime import date, timedelta
 
 import pytest
+
+from model import Batch, OrderLine, OutOfStock, allocate
 
 # ========================================================
 #  Fixtures
@@ -19,33 +19,48 @@ def sku_black_tshirt_large() -> str:
     return "BLACK-TSHIRT-LARGE"
 
 
+@pytest.fixture(scope="module")
+def today() -> date:
+    return date.today()
+
+
+@pytest.fixture(scope="module")
+def tomorrow() -> date:
+    return date.today() + timedelta(days=1)
+
+
+@pytest.fixture(scope="module")
+def later() -> date:
+    return date.today() + timedelta(weeks=1)
+
+
 # ========================================================
 #  Tests
 # ========================================================
 
 
 def test_can_allocate_if_available_greater_than_required(sku_red_tshirt_medium):
-    large_batch = Batch("batch-001", sku_red_tshirt_medium, date.today(), 10)
+    large_batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 4)
     assert large_batch.can_allocate(order_line)
 
 
 def test_can_allocate_if_available_equals_required():
-    batch = Batch(
-        "batch-001", sku_red_tshirt_medium, date.today(), 10
-    )
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 10)
     assert batch.can_allocate(order_line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required(sku_red_tshirt_medium):
-    small_batch = Batch("batch-001", sku_red_tshirt_medium, date.today(), 10)
+    small_batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 15)
     assert not small_batch.can_allocate(order_line)
 
 
-def test_cannot_allocate_returns_false_when_sku_does_not_match(sku_red_tshirt_medium, sku_black_tshirt_large):
-    batch = Batch("batch-001", sku_red_tshirt_medium, date.today(), 10)
+def test_cannot_allocate_returns_false_when_sku_does_not_match(
+    sku_red_tshirt_medium, sku_black_tshirt_large
+):
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_black_tshirt_large, 4)
     assert not batch.can_allocate(order_line)
 
@@ -53,15 +68,13 @@ def test_cannot_allocate_returns_false_when_sku_does_not_match(sku_red_tshirt_me
 def test_cannot_allocate_same_batch_twice(
     sku_red_tshirt_medium, sku_black_tshirt_large
 ):
-    batch = Batch("batch-001", sku_red_tshirt_medium, date.today(), 10)
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_black_tshirt_large, 1)
     assert not batch.can_allocate(order_line)
 
 
 def test_allocation_successful(sku_red_tshirt_medium):
-    batch = Batch(
-        "batch-001", sku_red_tshirt_medium, date.today(), 10
-    )
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 10)
     batch.allocate(order_line)
     assert batch.available_quantity == 0
@@ -69,9 +82,7 @@ def test_allocation_successful(sku_red_tshirt_medium):
 
 
 def test_deallocate(sku_red_tshirt_medium):
-    batch = Batch(
-        "batch-001", sku_red_tshirt_medium, date.today(), 10
-    )
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 10)
     batch.allocate(order_line)
     assert batch.available_quantity == 0
@@ -83,9 +94,7 @@ def test_deallocate(sku_red_tshirt_medium):
 
 
 def test_deallocate_order_line_not_allocated_to_batch():
-    batch = Batch(
-        "batch-001", sku_red_tshirt_medium, date.today(), 10
-    )
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 10)
     assert batch.available_quantity == 10
     assert order_line not in batch._allocated_order_lines
@@ -96,11 +105,49 @@ def test_deallocate_order_line_not_allocated_to_batch():
 
 
 def test_allocation_is_idempotent(sku_red_tshirt_medium):
-    batch = Batch(
-        "batch-001", sku_red_tshirt_medium, date.today(), 10
-    )
+    batch = Batch("batch-001", sku_red_tshirt_medium, 10, date.today())
     order_line = OrderLine("order-001", sku_red_tshirt_medium, 10)
     batch.allocate(order_line)
     batch.allocate(order_line)
     assert batch.available_quantity == 0
     assert order_line in batch._allocated_order_lines
+
+
+def test_prefers_warehouse_stock_batches_to_batches_in_transit(
+    sku_red_tshirt_medium, today
+):
+    warehouse_batch = Batch("warehouse-batch", sku_red_tshirt_medium, 10, None)
+    transit_batch = Batch("transit-batch", sku_red_tshirt_medium, 10, today)
+    order_line = OrderLine("order1", sku_red_tshirt_medium, 1)
+
+    allocation = allocate(order_line, [warehouse_batch, transit_batch])
+
+    assert allocation == "warehouse-batch"
+    assert warehouse_batch.available_quantity == 9
+    assert transit_batch.available_quantity == 10
+    assert order_line in warehouse_batch._allocated_order_lines
+
+
+def test_prefers_earlier_batches(sku_red_tshirt_medium, today, tomorrow, later):
+    fast_batch = Batch("fast-batch", sku_red_tshirt_medium, 10, today)
+    medium_batch = Batch("medium-batch", sku_red_tshirt_medium, 10, tomorrow)
+    slow_batch = Batch("slow-batch", sku_red_tshirt_medium, 10, later)
+    order_line = OrderLine("order1", sku_red_tshirt_medium, 1)
+
+    allocation = allocate(order_line, [fast_batch, medium_batch, slow_batch])
+
+    assert fast_batch.available_quantity == 9
+    assert medium_batch.available_quantity == 10
+    assert slow_batch.available_quantity == 10
+    assert order_line in fast_batch._allocated_order_lines
+    assert allocation == "fast-batch"
+
+
+def test_raises_out_of_stock_error_if_cannot_allocate(sku_red_tshirt_medium, today):
+    batch = Batch("fast-batch", sku_red_tshirt_medium, 10, today)
+    order_line_1 = OrderLine("order1", sku_red_tshirt_medium, 10)
+    allocate(order_line_1, [batch])
+
+    with pytest.raises(OutOfStock, match=sku_red_tshirt_medium):
+        order_line_2 = OrderLine("order2", sku_red_tshirt_medium, 1)
+        allocate(order_line_2, [batch])
